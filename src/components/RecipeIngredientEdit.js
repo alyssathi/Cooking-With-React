e70df790acba22678f8b83ecda1c3691150279f4
AlyssaThi <aruefenacht14@gmail.com>
2020-08-18 12:23:34 +1000

    edit functionality

---
+++ src/components/RecipeIngredientEdit.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// import Recipe from './Recipe'
+
+export default function RecipeIngredientEdit({ingredient, handleIngredientChange, handleIngredientDelete}) {
+
+    function handleChange(changes) {
+        handleIngredientChange(ingredient.id, { ...ingredient, ...changes})
+    }
+
+    return (
+        <>
+         <input 
+            className = "recipe-edit__input" 
+            value = {ingredient.name} 
+            onChange= {(e)=>  handleChange({name: e.target.value})}
+            type = "text" />
+         <input 
+            className = "recipe-edit__input" 
+            value = {ingredient.amount} 
+            onChange= {(e) => handleChange({amount: e.target.value})}
+            type = "text" />
+         <button className = "btn btn--danger"
+         onClick= {()=> handleIngredientDelete(ingredient.id)}>&times;</button>
+        </>
+    )
+}
